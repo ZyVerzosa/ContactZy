b5302c09f5c4ce92945e1a0d8bc59e71aee4b29c
ZyVerzosa <120782015+ZyVerzosa@users.noreply.github.com>
2024-12-05 14:57:20 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contact-btn').addEventListener('click', function () {
+    const phoneNumber = '+1234567890'; // Replace with your phone number
+    window.location.href = `tel:${phoneNumber}`;
+});
